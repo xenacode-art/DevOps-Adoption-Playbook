c3cb44476379033d2023ec8ae75a1a1d1e3b0518
xinacod <xinacod@Ubuntu22.myguest.virtualbox.org>
2023-09-04 14:33:17 +0100

    latest update 3.0.0

--- DevOps-Adoption-Playbook/TerraformTest/src/App.js
+++ DevOps-Adoption-Playbook/TerraformTest/src/App.js
@@ -8,13 +8,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React-App</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.          
         </p>
         <p className="App-intro">
-            <b>Build 2.0.0</b>
+            <b>Build 3.0.0</b>
         </p>
       </div>
     );
